83049c34867c4e622e63a1192c25ba4b19d993db
jakubbrom <jbrom@lanres.cz>
2025-03-06 16:34:29 +0100

    Change no. of points per sq. km to 200

--- get_random_points.py
+++ get_random_points.py
@@ -126,7 +126,7 @@ def get_vertices(polygon):
     return all_coords
 
 
-def generate_points_in_polygon(in_gdf_polygon, lake_buffer=-20, n_points_km=100, n_max_points=5000, **kwargs):
+def generate_points_in_polygon(in_gdf_polygon, lake_buffer=-20, n_points_km=200, n_max_points=5000, **kwargs):
     """
     Generate points within a polygon with respect of its complexity, and clip them with a buffer zone.
 
